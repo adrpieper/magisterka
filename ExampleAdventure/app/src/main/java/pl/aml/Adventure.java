e6ee6c07353db890247f876ad60ee0f9ca9c5960
Adrian Pieper <gergoybey@gmail.com>
2016-11-07 13:38:45 +0100

    fix Adventure class

--- ExampleAdventure/app/src/main/java/pl/aml/Adventure.java
+++ ExampleAdventure/app/src/main/java/pl/aml/Adventure.java
@@ -3,19 +3,10 @@ package pl.aml;
 public class Adventure {
 
     private final Location location;
-    private final Monster[] monsters;
 
-    public Adventure(Location location, Monster... monsters){
+
+    public Adventure(Location location){
 
         this.location = location;
-        this.monsters = monsters;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public Monster[] getMonsters() {
-        return monsters;
     }
 }
